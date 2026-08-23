a9f66949bd0d5276e9a047140beb6b97caa6b95f
anuj <anuj6202188@gmail.com>
2026-08-23 16:06:04 +0530

    added vercel frontend url in cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://nearly-nine.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
